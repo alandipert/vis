65cf8c9483f482fa9e894a09a7ce3944b3aee1ea
Alan Dipert <alan@dipert.org>
2017-11-17 12:24:47 -0800

    Apply Marc's patch to close windows from lua
    http://sprunge.us/BBBL

--- lua/visrc.lua
+++ lua/visrc.lua
@@ -9,3 +9,11 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- Your per window configuration options e.g.
 	-- vis:command('set number')
 end)
+
+vis:command_register("only", function(argv, force, cur_win, selection, range)
+	for win in vis:windows() do
+		if win ~= cur_win then
+			win:close(force)
+		end
+	end
+end)
